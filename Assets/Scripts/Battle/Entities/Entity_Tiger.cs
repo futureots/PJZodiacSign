a307e246dae02d70303e5d54ae5a663e2fa9ef49
futureots <futureots113@gmail.com>
2025-03-05 14:34:25 +0900

    tiger fix

--- Assets/Scripts/Battle/Entities/Entity_Tiger.cs
+++ Assets/Scripts/Battle/Entities/Entity_Tiger.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,10 @@ public class Entity_Tiger : Entity
         var list = new List<Tile>();
         for(int i = 0;i< 4; i++)
         {
-            for(int j = -1; j <= 1; i++)
+            for(int j = -1; j <= 1; j++)
             {
                 if (j == 0 && i == 0) continue;
-                var tile = field.GetTile(new intVector2(j, i));
+                var tile = field.GetTile(new intVector2(j, i) * negative + entityPos);
                 if(tile != null)
                 {
                     list.Add(tile);
