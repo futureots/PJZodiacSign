739c7f489dc2dd6cf104ea6fbac6e824db40c409
futureots <futureots113@gmail.com>
2025-03-30 02:11:14 +0900

    little edit

--- Assets/Scripts/Battle/Entities/JodiacDataSO.cs
+++ Assets/Scripts/Battle/Entities/JodiacDataSO.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "JodiacData",menuName = "JodiacData")]
 public class JodiacDataSO : ScriptableObject
 {
+    // ¸ðµ¨ ¸Å½Ã
+    public Mesh mesh;
+
+    public Material[] elementMaterial;
     public int hp;
     public int hpIncrease;
     public int power;
     public int powerIncrease;
     public List<SkillData> skills;
-    
+
+    public List<Tile> GetAttackArea(Field field) { return null; }
 }
+
 [System.Serializable]
 public struct SkillData
 {
