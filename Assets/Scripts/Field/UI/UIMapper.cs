356d3ca12f6917ef7937ce7c3f54e0e2625ac427
Jaehyun9912 <limit8802@gmail.com>
2026-01-02 01:35:00 +0900

    UI 타입 배치에 따른 턴 별 UI 활성 세팅중

--- Assets/Scripts/Field/UI/UIMapper.cs
+++ Assets/Scripts/Field/UI/UIMapper.cs
@@ -1,27 +1,52 @@
-
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
+[Flags]
 public enum UIType
 {
-    Inventory,
-    PlayerData,
-    EntityInfo,
-    Turn,
-    Option,
-    Shop,
-    Resource,
+    None,
+    PlayerData = 1 << 0,
+    EntityInfo =  1 << 1,
+    Turn = 1 << 2,
+    Option = 1 << 3,
+    Shop =  1 << 4,
+    Resource = 1 << 5,
+    Log = 1 << 6,
+    Inventory =  1 << 7,
 }
 
 [Serializable]
 public struct UIMap
 {
     public UIType type;
-    public GameObject uiPanel;
+    public GameObject uiPanel;      // NOTE: UI Panel 추상 타입으로 개선
 }
 
 public class UIMapper
 {
+    [SerializeField] private List<UIMap> uiMaps;
+    private Dictionary<UIType, GameObject> container = new();
+
+    /// <summary>
+    /// Refresh UI Container Dictionary
+    /// </summary>
+    private void RefreshDictionary()
+    {
+        // Reset Container
+        container.Clear();
+
+        // Bake Dictionary
+        foreach (var ui in uiMaps)
+        {
+            if (ui.uiPanel && !container.ContainsKey(ui.type))
+            {
+                container.Add(ui.type, ui.uiPanel);
+                
+                ui.uiPanel.SetActive(false);        // Default Inactive
+            }
+        }
+    }
+    
     
 }
