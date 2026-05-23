7628fb1339e1aaac1e41e91fefe5f288bdebcc7b
futureots <futureots113@gmail.com>
2026-05-24 01:38:38 +0900

    fix : 일부 밸런스 수정

--- Assets/Scripts/Field/FieldController.cs
+++ Assets/Scripts/Field/FieldController.cs
@@ -193,7 +193,7 @@ public class FieldController : MonoBehaviour
     public virtual void ContinueGame()
     {
         var credit = Math.Min(localPlayer.Credit,200);
-        localPlayer.Credit += 200 + Mathf.RoundToInt(credit*0.2f);
+        localPlayer.Credit += 150 + Mathf.RoundToInt(credit*0.2f);
         OnBattleEnd?.Invoke("CLEAR");
     }
     #endregion
